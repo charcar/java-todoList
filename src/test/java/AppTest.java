30023d24c835eca7b4141543e078215572af7365
Charles Carter and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-02-16 10:06:36 -0800

    add tasks into an arraylist

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -41,4 +41,17 @@ public class AppTest extends FluentTest {
     click("a", withText("Go Back"));
     assertThat(pageSource()).contains("Mow the lawn");
   }
-} 
+
+  @Test
+  public void multipleTasksAreDisplayedTest() {
+    goTo("http://localhost:4567/");
+    fill("#description").with("Mow the lawn");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    fill("#description").with("Buy groceries");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    assertThat(pageSource()).contains("Mow the lawn");
+    assertThat(pageSource()).contains("Buy groceries");
+  }
+}
